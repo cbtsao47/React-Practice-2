b7dc4cd197a3e0c160db8eacebd19664f0711a11
cbtsao47 <cbtsao47@gmail.com>
2018-11-20 15:14:18 -0600

    add nested components

--- day2/src/App.js
+++ day2/src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+const GrandChild = props => {
+  const { personData } = props;
+  return <h3>{personData.grandChildName}</h3>;
+};
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Child = props => {
+  const { personData } = props;
+  return (
+    <div>
+      <h2>{personData.childName}</h2>
+      <GrandChild personData={personData} />
+    </div>
+  );
+};
+const Parent = props => {
+  const { personData } = props;
+  return (
+    <div>
+      <h1>Parent: {personData.parentName}</h1>
+      <Child personData={personData} />
+    </div>
+  );
+};
 
+const App = () => {
+  const personData = {
+    parentName: "Gilbert",
+    childName: "Regina",
+    grandChildName: "Ryan"
+  };
+
+  return (
+    <div>
+      <Parent personData={personData} />
+    </div>
+  );
+};
 export default App;
